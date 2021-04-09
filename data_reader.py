9a33ee38f24ae301f75e9a31df162635fd5aa3ec
jakwiat <kubakwiat123@gmail.com>
2021-04-09 23:15:34 +0200

    moved data reading to separate function

--- data_reader.py
+++ data_reader.py
@@ -1,18 +1,22 @@
 import csv
 
-with open('final_train.csv') as f:
-    csv_reader = csv.reader(f, delimiter=',')
-    line_count = 0
-    data = []
-    for row in csv_reader:
-        if line_count == 0:
-            z = f'Column names are {", ".join(row)}'
 
+def data_reader(filename):
+    with open(filename) as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        line_count = 0
+        data = []
+        for row in csv_reader:
+            if line_count == 0:
+                attribute_names = row[1:]
+            else:
+                data.append(row[1:])
             line_count += 1
-        else:
-            data.append(row)
-            line_count += 1
-        print(line_count)
-    print(z)
-    print(f'Processed {line_count} lines.')
-    print(data[0])
+            print(line_count)
+        print(attribute_names)
+        print(data[0])
+
+        return attribute_names, data
+
+
+attribute_names, data = data_reader('final_train.csv')
