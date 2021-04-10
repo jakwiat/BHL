2ab857e855b8bd1ee5ccae5484c2ad03881070e1
gkaczm <gabriela.kaczmarek99@gmail.com>
2021-04-10 12:45:54 +0200

    filling data, testing rand forest classification

--- data_reader.py
+++ data_reader.py
@@ -1,32 +1,32 @@
-import csv
-import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 import pandas as pd
-
-def data_reader(filename):
-    with open(filename) as f:
-        csv_reader = csv.reader(f, delimiter=',')
-        line_count = 0
-        data = []
-        for row in csv_reader:
-            if line_count == 0:
-                attribute_names = row[1:]
-            else:
-                data.append(row[1:])
-            line_count += 1
-        print(line_count)
-        print(attribute_names)
-        print(data[0])
-
-        return attribute_names, data
 
 
 df = pd.read_csv('final_train.csv')
-#attribute_names, data = data_reader('final_train.csv')
 df = df.iloc[:,1:]
 klasy = df.loc[:,"Activity"]
 print(f" Classes: {set(klasy)} ")
 print(klasy.value_counts(), end="\n\n")
 time_series_cols = [col for col in df if col.startswith('t') or col == "Activity"]
 print(df[time_series_cols].head())
+colnames = []
+for col in df[time_series_cols].columns:
+    colnames.append(col)
+print(df.isna().sum().sum())
+df = df.fillna(df.mean())
+print(df.isna().sum().sum())
+vals = df.iloc[:,1:]
+tags = df.loc[:, "Activity"]
+print(vals, tags)
+v_train, v_test, t_train, t_test = train_test_split(vals, tags, test_size=0.3)
+# Create a Gaussian Classifier
+clf = RandomForestClassifier(n_estimators=100)
+# Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(v_train, t_train)
+t_pred = clf.predict(v_test)
 
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(t_test, t_pred))
 
